83df4ed9924acda2e4cd8f4ff73d328fd83b456d
krawj <jacekkr@gmail.com>
2025-01-26 16:41:54 +0100

    clean up main() and add generate_report()

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-    print(count_words(file_contents))
-    print(count_chars(file_contents))
+    file_path = "books/frankenstein.txt"
+    file_contents = open_book(file_path)
+    word_count = count_words(file_contents)
+    character_count = count_chars(file_contents)
+    character_count.sort(reverse=True, key=sort_on)
+    generate_report(file_path, word_count, character_count)
+
+def open_book(path):
+    with open(path) as f:
+        return f.read()
 
 def count_words(string):
     words = string.split()
     return len(words)
 
 def count_chars(string):
-    input = string.lower()
+    chars = {}
     
-    unique_chars = set()
-    char_counts = {}
+    for char in string:
+        lowercase = char.lower()
+        if char.isalpha():
+            if lowercase in chars:
+                chars[lowercase] += 1
+            else:
+                chars[lowercase] = 1
+    
+    char_count_list = [{'name': key, 'num': value} for key, value in chars.items()]
 
-    for char in input:
-        unique_chars.add(char)
+    return char_count_list
 
-    for char in unique_chars:
-        count = 0
-        for letter in input:
-            if letter == char:
-                count += 1
-        char_counts[char] = count
-    return char_counts
+def sort_on(dict):
+    return dict["num"]
+
+def generate_report(book_file, word_count, character_counts):
+    print(f"--- Begin report on {book_file} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+
+    for dict in character_counts:
+        print(f"The character '{dict['name']}' was found {dict['num']} times")
+
+    print("")
+    print("--- End report ---")
 
 main()
